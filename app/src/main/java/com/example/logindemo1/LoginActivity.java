61d233934cdf203f71ce9583c9f285b5374fa6be
Phani Kumar <phani.k@synup.com>
2021-05-06 10:18:09 +0530

    Updated with android suggested fixes

--- app/src/main/java/com/example/logindemo1/LoginActivity.java
+++ app/src/main/java/com/example/logindemo1/LoginActivity.java
@@ -1,29 +1,24 @@
 package com.example.logindemo1;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
-    private Button button_register;
-    private Button button_login;
     private FirebaseAuth auth;
     private String txt_email;
     private String txt_password;
@@ -36,75 +31,63 @@ public class LoginActivity extends AppCompatActivity {
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-        button_register = findViewById(R.id.button_register);
-        button_login = findViewById(R.id.button_login);
+        Button button_register = findViewById(R.id.button_register);
+        Button button_login = findViewById(R.id.button_login);
         auth = FirebaseAuth.getInstance();
         txt_email = email.getText().toString();
         txt_password = password.getText().toString();
 
-        button_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txt_email = email.getText().toString();
-                txt_password = password.getText().toString();
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
-                    Toast.makeText(LoginActivity.this, "Missing Credentials", Toast.LENGTH_SHORT).show();
-                } else if (txt_password.length() < 6) {
-                    Toast.makeText(LoginActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
-                } else {
-                    loginUser(txt_email, txt_password);
-                }
+        button_login.setOnClickListener(v -> {
+            txt_email = email.getText().toString();
+            txt_password = password.getText().toString();
+            if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                Toast.makeText(LoginActivity.this, "Missing Credentials", Toast.LENGTH_SHORT).show();
+            } else if (txt_password.length() < 6) {
+                Toast.makeText(LoginActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
+            } else {
+                loginUser(txt_email, txt_password);
             }
         });
 
-        button_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txt_email = email.getText().toString();
-                txt_password = password.getText().toString();
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
-                    Toast.makeText(LoginActivity.this, "Missing Credentials!", Toast.LENGTH_SHORT).show();
-                } else if (txt_password.length() < 6) {
-                    Toast.makeText(LoginActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
-                } else {
-                    registerUser(txt_email, txt_password);
-                }
+        button_register.setOnClickListener(v -> {
+            txt_email = email.getText().toString();
+            txt_password = password.getText().toString();
+            if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                Toast.makeText(LoginActivity.this, "Missing Credentials!", Toast.LENGTH_SHORT).show();
+            } else if (txt_password.length() < 6) {
+                Toast.makeText(LoginActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
+            } else {
+                registerUser(txt_email, txt_password);
             }
         });
     }
 
     private void loginUser(String email, String password) {
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    startActivity(new Intent(LoginActivity.this, StudentAdmissions.class));
-                    finish();
-                } else {
-                    try {
-                        throw task.getException();
-                    } catch (Exception e) {
-                        Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
-                    }
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, task -> {
+            if (task.isSuccessful()) {
+                startActivity(new Intent(LoginActivity.this, StudentAdmissions.class));
+                finish();
+            } else {
+                try {
+                    throw Objects.requireNonNull(task.getException());
+                } catch (Exception e) {
+                    Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
     private void registerUser(String email, String password) {
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(LoginActivity.this, "Successful!", Toast.LENGTH_SHORT).show();
-                } else {
-                    try {
-                        throw task.getException();
-                    } catch(FirebaseAuthUserCollisionException e) {
-                        Toast.makeText(LoginActivity.this, "User already exist!", Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
-                        Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
-                    }
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(LoginActivity.this, "Successful!", Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    throw Objects.requireNonNull(task.getException());
+                } catch(FirebaseAuthUserCollisionException e) {
+                    Toast.makeText(LoginActivity.this, "User already exist!", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
--- app/src/main/java/com/example/logindemo1/StudentAdmissions.java
+++ app/src/main/java/com/example/logindemo1/StudentAdmissions.java
@@ -6,38 +6,29 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
-
 import com.google.firebase.auth.FirebaseAuth;
 
-
 public class StudentAdmissions extends AppCompatActivity {
-
-    private Toolbar toolbar;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_admissions);
         setTitle("Login");
 
-        toolbar = findViewById(R.id.main_toolbar);
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if(item.toString().equals("Logout")) {
-                    FirebaseAuth.getInstance().signOut();
-                    startActivity(new Intent(StudentAdmissions.this, LoginActivity.class));
-                    finish();
-                    return true;
-                } else {
-                    return false;
-                }
+        toolbar.setOnMenuItemClickListener(item -> {
+            if(item.toString().equals("Logout")) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(StudentAdmissions.this, LoginActivity.class));
+                finish();
+                return true;
+            } else {
+                return false;
             }
         });
+
     }
 
     @Override
